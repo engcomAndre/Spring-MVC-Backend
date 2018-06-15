69fb3b17e83097c67bd6728b4e02d47bcb6b6bac
André Vieira da Silva - HOME <sgavsnake@gmail.com>
2018-06-14 22:29:15 -0300

    Produto and Categoria OK

--- _cursomc/src/main/java/com/andre/cursomc/domain/Produto.java
+++ _cursomc/src/main/java/com/andre/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 public class Produto implements Serializable{
@@ -25,6 +27,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco; 
 	
+	@JsonManagedReference
 	@ManyToMany
 	@JoinTable(name="PRODUTO_CATEGORIA",
 			   joinColumns = @JoinColumn(name="produto_id"),
@@ -90,10 +93,6 @@ public class Produto implements Serializable{
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
 	}
-	
-	
-	
-	
 	
 	
 
