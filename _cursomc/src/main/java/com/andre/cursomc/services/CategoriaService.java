da915cea3415f31ca3ccffcb5d36f1d4061e27da
André Vieira da Silva - IVIA-UNI7 <sgavsnake@gmail.com>
2018-06-18 12:05:08 -0300

    Pagination for a class Categorias implemented

--- _cursomc/src/main/java/com/andre/cursomc/services/CategoriaService.java
+++ _cursomc/src/main/java/com/andre/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andre.cursomc.domain.Categoria;
@@ -44,10 +47,14 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
-	}
-	
+	}	
 	public List<Categoria> findAll() {
 		return repo.findAll();		
+	}
+	
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
