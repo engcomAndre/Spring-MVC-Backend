19e946f073ccf395dc8078a49a0b68daa41ac03e
André Vieira da Silva - IVIA-UNI7 <sgavsnake@gmail.com>
2018-06-18 11:24:30 -0300

    Add Listing Categoria object

--- _cursomc/src/main/java/com/andre/cursomc/resources/CategoriaResource.java
+++ _cursomc/src/main/java/com/andre/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.andre.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.cursomc.domain.Categoria;
+import com.andre.cursomc.dto.CategoriaDTO;
 import com.andre.cursomc.services.CategoriaService;
 
 @RestController
@@ -49,6 +53,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();		
+	}
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List <Categoria> categorias = service.findAll();
+		List<CategoriaDTO> categoriasDTO = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriasDTO);
+		
 	}
 }
 
