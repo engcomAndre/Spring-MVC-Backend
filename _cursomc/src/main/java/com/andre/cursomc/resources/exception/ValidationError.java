10aff47925a0bd53447c728ef28b18cc06e71e9f
André Vieira da Silva - IVIA-UNI7 <sgavsnake@gmail.com>
2018-06-19 10:08:30 -0300

    Validation Return personalization

---
+++ _cursomc/src/main/java/com/andre/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,25 @@
+package com.andre.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+		public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName,String message) {
+		this.errors.add(new FieldMessage(fieldName,message));
+	}
+
+	public ValidationError(Integer status, Long timeStamp, String msg) {
+		super(status, timeStamp, msg);
+	}
+
+}
