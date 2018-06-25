00baf7ce9bf79ac694e268bf3e6ae84b1106d942
André Vieira da Silva - IVIA-UNI7 <sgavsnake@gmail.com>
2018-06-25 12:41:56 -0300

    Created a test profile

--- _cursomc/src/main/java/com/andre/cursomc/resources/PedidoResource.java
+++ _cursomc/src/main/java/com/andre/cursomc/resources/PedidoResource.java
@@ -1,26 +1,20 @@
 package com.andre.cursomc.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.cursomc.domain.Pedido;
-import com.andre.cursomc.domain.Produto;
-import com.andre.cursomc.dto.ProdutoDTO;
-import com.andre.cursomc.resources.utils.URL;
 import com.andre.cursomc.services.PedidoService;
 
 @RestController
